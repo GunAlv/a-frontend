b6f220eda76fc29d8a4afc7afca472c96cf07dcd
Gunay <lambertalv@gmail.com>
2024-12-24 17:29:06 +0700

    1

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { memo } from 'react';
 console.log('key: ', import.meta.env.VITE_MAP_API_KEY);
 
 export const App = memo(() => {
-  return 'app';
+  return 'app 1';
 });
 
 App.displayName = 'App';
